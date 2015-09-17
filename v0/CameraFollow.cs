aa10e5d54df36f649240773c741f830422d51490
premacaa <premacaa@sheridanc.on.ca>
2015-09-16 22:42:24 -0400

    Version 0
    Starting point for game

---
+++ v0/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public PlayerController player;
+    private Vector3 playerPos;
+    private float offset;
+
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<PlayerController>();
+        playerPos = player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        offset = player.transform.position.x - playerPos.x;
+        transform.position = new Vector3(transform.position.x + offset, transform.position.y, transform.position.z); 
+        playerPos = player.transform.position;
+	}
+}
---
+++ v0/ScrollingBackground.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingBackground : MonoBehaviour {
+
+    public float scrollingSpeed;
+    public float tile;
+    private Vector3 start;
+
+
+	// Use this for initialization
+	void Start () {
+        start = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float newPos = Mathf.Repeat(Time.time * scrollingSpeed, tile);
+        transform.position = start + Vector3.left * newPos;
+    
+    }
+
+}
